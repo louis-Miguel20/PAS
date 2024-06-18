c14caeb831f8dc128684b17f0407f8db9e310881
Luis Miguel Ra <luismiguelraaaa@gmail.com>
2024-06-17 22:14:05 -0500

    avances de diseño

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -28,7 +28,7 @@ export class LoginComponent implements OnInit {
   createForm(){
     this.formu=this.form.group({
       email    : ["", [Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")],[]],
-      password : ["", [Validators.required,Validators.minLength(6)],[]]
+      password : ["", [Validators.required,Validators.minLength(1)],[]]
     })
   }
 
